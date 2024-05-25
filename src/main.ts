a3023045520e2fc7bf24f52d305c256c3075e5aa
Uzair Abdullah <uzairabdullah973@gmail.com>
2024-05-25 22:12:17 +0500

    changed port

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 
+const port = process.env.PORT || 5000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
-  await app.listen(5000);
+  await app.listen(port);
 }
 bootstrap();
